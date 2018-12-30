e219f3a51c79d4bf2d36ca103687d27601c055ff
Justin Boey <jboey@kent.edu>
2018-12-30 00:46:32 -0500

    Add camera to app (and it still runs!) but camera doesnt take pictures for some reason

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
+import { Camera } from '@ionic-native/camera/ngx';
+import { Options } from 'selenium-webdriver/safari';
 
 @NgModule({
   imports: [
@@ -12,6 +14,9 @@ import { Tab2Page } from './tab2.page';
     FormsModule,
     RouterModule.forChild([{ path: '', component: Tab2Page }])
   ],
+  providers: [
+    Camera
+  ] ,
   declarations: [Tab2Page]
 })
 export class Tab2PageModule {}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
-// import { Camera } from 'ionic-native'
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  public base64Image: string;
+
+  constructor(private camera: Camera) {}
+
+  public takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
+     }, (err) => {
+      console.log(err);
+     });
+  }
+}
